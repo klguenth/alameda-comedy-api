9985347838edc5993b97c5220c31b1550e8fd80e
klguenth <klguenth@gmail.com>
2020-05-05 20:57:53 -0500

    update

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const {CLIENT_ORIGIN} = require('./config')
+const knex = require('knex');
 
 const app = express()
 
@@ -12,10 +13,19 @@ const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
+const db = knex({
+    client: 'pg',
+    connection: DATABASE_URL,
+})
+
+app.set('db', db)
+
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
-
+app.use('*', (req, res, next) => {
+    res.status(404).send('It ain\'t here!');
+})
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
