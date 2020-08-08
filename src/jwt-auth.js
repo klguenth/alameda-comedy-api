3f8ccb327ed6e86c4b405965f0c43090f8a78373
klguenth <klguenth@gmail.com>
2020-08-08 11:15:54 -0500

    update

--- src/jwt-auth.js
+++ src/jwt-auth.js
@@ -8,27 +8,6 @@ function requireAuth(req, res, next) {
     } else {
         bearerToken = authToken.slice(7, authToken.length)
     }
-
-    // const [tokenUserName, tokenPassword] = Buffer
-    //     .from(basicToken, 'base64')
-    //     .toString()
-    //     .split(':')
-
-    // if (!tokenUserName || !tokenPassword) {
-    //     console.log('request two')
-    //     return res.status(401).json({ error: 'Unauthorized request' })
-    // }
-    // req.app.get('db')('users')
-    //     .where({ email: tokenUserName })
-    //     .first()
-    //     .then(user => {
-    //         if (!user) {
-    //             console.log('request three')
-    //             return res.status(401).json({ error: 'Unauthorized request' })
-    //         }
-    //         next()
-    //     })
-    //     .catch(next)
     try {
         const payload = AuthService.verifyJwt(bearerToken)
         AuthService.getUserWithUserName(
@@ -49,35 +28,6 @@ function requireAuth(req, res, next) {
         res.status(401).json({ error: 'Unauthorized request two' })
     }
 }
-
-// function requireJWTAuth(req, res, next) {
-//     const authToken = req.get('Authorization') || ''
-
-//     let bearerToken
-//     if (!authToken.toLowerCase().startsWith('bearer ')) {
-//         return res.status(401).json({ error: 'Missing bearer token' })
-//     } else {
-//         bearerToken = authToken.slice(7, authToken.length)
-//     }
-
-//     const [tokenUserName, tokenPassword] = Buffer
-//         .from(bearerToken, 'jwt')
-//         .toString()
-//         .split(':')
-
-//     if (!tokenUserName || !tokenPassword) {
-//         return res.status(401).json({ error: 'Unauthorized request' })
-//     }
-//     req.app.get('db')('users')
-//         .where({ email: tokenUserName })
-//         .first()
-//         .then(user => {
-//             if (!user) {
-//                 return res.status(401).json({ error: 'Unauthorized request' })
-//             }
-//             next()
-//         })
-//         .catch(next)
 
 module.exports = {
     requireAuth,
