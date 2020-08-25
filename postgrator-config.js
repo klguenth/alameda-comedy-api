193ca141a7f12767128fcbb2c7a56cfb721680e1
klguenth <klguenth@gmail.com>
2020-08-24 20:18:24 -0500

    postgrator fix attempt

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
-const Postgrator = require('postgrator')
 
-const postgrator = new Postgrator({
+module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg",
     "connectionString": (process.env.NODE_ENV === 'test')
@@ -9,20 +8,4 @@ const postgrator = new Postgrator({
         : process.env.DATABASE_URL,
     "ssl": !!process.env.SSL,
     "validateChecksums": false,
-    "database": (process.env.NODE_ENV === 'test')
-        ? "alamedacomedy-test"
-        : "alamedacomedy",
-    "username": "alamedacomedy",
-    "password": "alamedacomedy",
-    "schemaTable": "alameda.comedyschema"
-})
-
-postgrator
-    .migrate('003')
-    .then((appliedMigrations) => console.log(appliedMigrations))
-    .catch((error) => {
-        console.log(error)
-        // Because migrations prior to the migration with error would have run
-        // error object is decorated with appliedMigrations
-        console.log(error.appliedMigrations) // array of migration objects
-    })
+}
