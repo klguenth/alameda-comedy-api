f3553fd2edac2eaa5e664bb330732fada840ac50
klguenth <klguenth@gmail.com>
2020-08-31 20:42:45 -0500

    update PORT value

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
